b9ef7e9c57741f4305d31de783c825bf2180664e
TristanMacaulayMK <tristan.macaulay@mightykingdom.com>
2023-10-03 09:17:10 +1100

    Created a gameplay module.

---
+++ Source/MerinoGameplay/MerinoGameplay.Build.cs
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+using UnrealBuildTool;
+
+public class MerinoGameplay : ModuleRules
+{
+	public MerinoGameplay(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine"});
+	}
+}
